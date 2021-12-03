1875e311679460b8856dcb632cca0445e82036a2
Titusz Ban <tituszban@gmail.com>
2021-12-03 12:52:13 +0000

    Add reusable boilerplate

---
+++ main.py
@@ -0,0 +1,12 @@
+def solve(test_input):
+    pass
+
+def main():
+    with open("") as f:
+        test_input = list(map(lambda l: l.strip(), f.readlines()))
+    
+    result = solve(test_input)
+    print(result)
+
+if __name__ == "__main__":
+    main()
