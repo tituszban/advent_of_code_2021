fdc9d38a35766abfd092688aa13a97c1caa4ad55
Titusz Ban <tituszban@gmail.com>
2021-12-08 08:20:49 +0000

    Day 8 2 WIP

--- 08_2/main.py
+++ 08_2/main.py
@@ -29,119 +29,68 @@ class Digit:
         return self.s == other.s
 
 
-class DigitConfig:
+class ValidDigit(Digit):
     digit_configs = {
-        8: "abcdefg",
-        0: "abcefg",
-        6: "abdefg",
-        9: "abcdfg",
-        2: "acdeg",
-        3: "abdfg",
-        5: "abdfg",
-        4: "bcdf",
-        7: "acf",
-        1: "cf",
-    }
-    digit_configs_by_len = {
-        7: [8],
-        6: [0, 6, 9],
-        5: [2, 3, 5],
-        4: [4],
-        3: [7],
-        2: [1]
+        "abcdefg": 8,
+        "abcefg": 0,
+        "abdefg": 6,
+        "abcdfg": 9,
+        "acdeg": 2,
+        "acdfg": 3,
+        "abdfg": 5,
+        "bcdf": 4,
+        "acf": 7,
+        "cf": 1,
     }
 
+    def __init__(self, s):
+        super().__init__(s)
+        if self.s not in self.digit_configs.keys():
+            raise Exception(f"Invalid digit {self.s}")
+
+    @classmethod
+    def is_valid(cls, s):
+        return ''.join(sorted(s)) in cls.digit_configs.keys()
+
+    def decode(self):
+        return self.digit_configs[self.s]
+
+
+class DigitMapping:
+    def __init__(self, mapping):
+        self.mapping = mapping
+
+    def apply_mapping(self, scrambled_digit):
+        s = ''.join(self.mapping[c] for c in scrambled_digit.s)
+        if ValidDigit.is_valid(s):
+            return ValidDigit(s)
+        return None
+
+
+class DigitConfig:
     def __init__(self, seq):
         reference, output = seq.split(" | ")
         self.reference = list(map(Digit, reference.split()))
         self.output = list(map(Digit, output.split()))
 
-    def decode2(self):
-        def apply_mapping(digit, mapping):
-            return frozenset(mapping[s] for s in digit.s)
-
-        correct_mapping = {"d": "a", "e": "b", "a": "c", "f": "d", "g": "e", "b": "f", "c": "g"}
-
-        digits = set(frozenset(v) for v in self.digit_configs.values())
+    def decode(self):
         chars = "abcdefg"
         for m in it.permutations(chars, 7):
-            mapping = {o: g for o, g in zip(m, chars)}
-            mapped = [apply_mapping(d, mapping) for d in [*self.reference, *self.output]]
-            is_consistent = all(m in digits for m in mapped)
-            if is_consistent:
-                print(mapping)
-        return 0
+            mapping = DigitMapping({o: g for o, g in zip(m, chars)})
 
-    def decode(self):
-        digits_by_len = {i: set([]) for i in range(2, 8)}
-        for digit in [*self.reference, *self.output]:
-            digits_by_len[len(digit)].add(digit)
+            mapped = [mapping.apply_mapping(d)
+                      for d in [*self.reference, *self.output]]
+            is_consistent = all(m for m in mapped)
+            if not is_consistent:
+                continue
+            
+            mapped_output = [
+                mapping.apply_mapping(o).decode()
+                for o in self.output
+                ]
+            return int(''.join(map(str, mapped_output)))
 
-        digits_with_possible_digits = {}
-
-        for l, digits in digits_by_len.items():
-            possible_digits = self.digit_configs_by_len[l]
-            for digit in digits:
-                digits_with_possible_digits[digit] = possible_digits
-
-        def get_all_configs(_dwpd):
-            keys = list(_dwpd.keys())
-            key = keys.pop(0)
-            for p in _dwpd[key]:
-                if not any(keys):
-                    yield {key: p}
-                else:
-                    for c in get_all_configs({k: _dwpd[k] for k in keys}):
-                        yield {key: p, **c}
-
-        possible_configurations = list(
-            get_all_configs(digits_with_possible_digits))
-
-        def is_mapping_valid(mapping, digit, config):
-            c = self.digit_configs[config]
-
-            return all(mapping[x] in c for x in digit.s if x in mapping)
-
-        def is_consistent(config, mappings={}):
-            digits = list(config.keys())
-            digit = digits.pop(0)
-
-            if not is_mapping_valid(mappings, digit, config[digit]):
-                return
-
-            unmapped_digits = [d for d in digit.s if d not in mappings]
-
-            for p in it.permutations(unmapped_digits):
-                mapping = {o: g for o, g in zip(
-                    p, self.digit_configs[config[digit]])}
-                if not any(digits):
-                    yield {**mappings, **mapping}
-                else:
-                    c = list(is_consistent(
-                        {d: config[d] for d in digits}, mappings={**mappings, **mapping}))
-
-                    if not any(c):
-                        continue
-                    yield from c
-
-        for possible_config in possible_configurations:
-            c = list(is_consistent(possible_config))
-            if any(c):
-                print(c)
-        print(digits_by_len)
-
-        """
-        8: 7; abcdefg
-        0: 6; abc efg
-        6: 6; ab defg
-        9: 6; abcd fg
-        2: 5; a cde g
-        3: 5; a bd fg
-        5: 5; ab d fg
-        4: 4;  bcd f 
-        7: 3; a c  f 
-        1: 2;   c  f 
-        """
+        raise Exception("No mapping found")
 
     def count_known_digits_based_on_length(self):
         return sum([1 for digit in self.output if digit.decode_length() is not None])
@@ -149,7 +98,7 @@ class DigitConfig:
 
 def solve(test_input):
     configs = map(DigitConfig, test_input)
-    return sum(config.decode2() for config in configs)
+    return sum(config.decode() for config in configs)
 
 
 example_input = """
@@ -174,7 +123,7 @@ def main():
     with open("08_1/input.txt") as f:
         test_input = list(map(lambda l: l.strip(), f.readlines()))
 
-    result = solve(example_input_0)
+    result = solve(test_input)
     print(result)
 
 
